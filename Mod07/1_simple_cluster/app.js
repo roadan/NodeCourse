e165457c7cede38597f28a2c7adabda261fe33c4
Yaniv Rodenski <roadan@gmail.com>
2014-09-22 15:11:52 +0300

    added an IPC cluster demo

--- Mod07/1_simple_cluster/app.js
+++ Mod07/1_simple_cluster/app.js
@@ -1,20 +1,23 @@
 var cluster = require('cluster');
-var http = require('http');
+var http 	= require('http');
 var numCPUs = require('os').cpus().length;
 
 if (cluster.isMaster) {
-		// Fork workers.
-		for (var i = 0; i < numCPUs; i++) {
-			cluster.fork();
-		}
+	
+	// Fork workers.
+	for (var i = 0; i < numCPUs; i++) {
+		cluster.fork();
+	}
 
-		cluster.on('listening', function(worker, address) {
+	// the cluster api is also an event emitter 
+	cluster.on('listening', function(worker, address) {
 		console.log('worker ' + worker.process.pid + ' is listening on ' + address.address + ':' + address.port);
 	});
 } else {
-		// Workers can share any TCP connection
-		// In this case its a HTTP server
-		http.createServer(function(req, res) {
+
+	// Workers can share any TCP connection
+	// In this case its a HTTP server
+	http.createServer(function(req, res) {
 		res.writeHead(200);
 		res.end('worker ' + cluster.worker.process.pid + ' returend the result \n');
 	}).listen(8000);
---
+++ Mod07/2_IPC/app.js
@@ -0,0 +1,26 @@
+var cluster = require('cluster');
+var http 	= require('http');
+var numCPUs = require('os').cpus().length;
+
+if (cluster.isMaster) {
+	
+	// Fork workers.
+	for (var i = 0; i < numCPUs; i++) {
+		cluster.fork();
+	}
+
+	// sending an IPC message
+	for (var id in cluster.workers) {
+		cluster.workers[id].send('Hi, you are worer id:' + id);
+	}
+	
+} else {
+
+	process.on('message', function messageHandler(msg) {
+
+	//	console.log(cluster.isMaster);
+		console.log(msg);
+	
+	});
+
+}
