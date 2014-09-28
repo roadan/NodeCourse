dd5e907f4b3996af4c9ddbf69e54c9c1c4fee53d
Nir Noy <noynir@users.noreply.github.com>
2014-09-28 22:51:42 +0300

    added package.json
    require colors

--- Mod01/3_FileSystem/list.js
+++ Mod01/3_FileSystem/list.js
@@ -3,11 +3,9 @@ var colors = require('colors');
 
 // this method reads the content of a directory
 fs.readdir(process.argv[2], function(err,files){
-
-
     files.forEach(function(file){
 
-        var path = process.argv[2] + '\/' + file;
+        var path = process.argv[2] + '\\' + file;
 
         fs.stat(path, function(err, stats){
             if(stats != undefined){
