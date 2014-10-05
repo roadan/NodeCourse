19847c3b01c6f0fcb753b7da6d1cb885b376442a
Nir Noy <noynir@users.noreply.github.com>
2014-10-06 01:39:49 +0300

    clean up

--- Mod08/1_async/final/async_each.js
+++ Mod08/1_async/final/async_each.js
@@ -30,20 +30,3 @@ async.each(items,function(item,callback){
   doSomethingOnceAllAreDone();
 });
 
-  
-// // 1st para in async.each() is the array of items
-// async.each(items,
-//   // 2nd param is the function that each item is passed to
-//   function(item, callback){
-//     // Call an asynchronous function, often a save() to DB
-//     item.someAsyncCall(function (){
-//       // Async call is done, alert via callback
-//       callback();
-//     });
-//   },
-//   // 3rd param is the function to call when everything's done
-//   function(err){
-//     // All tasks are done now
-//     doSomethingOnceAllAreDone();
-//   }
-// );
